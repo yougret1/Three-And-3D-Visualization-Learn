98b45d3bf2cd07911a1a6bb0acc78a69bac93194
yougret1 <1935109257@qq.com>
2024-02-19 16:50:17 +0800

    fix: 相机位置添加

--- 18-智慧城市/代码/threeapp/src/three/camera.js
+++ 18-智慧城市/代码/threeapp/src/three/camera.js
@@ -1,7 +1,8 @@
 import * as THREE from 'three'
 
 const camera = new THREE.PerspectiveCamera(
-  45 , window.innerWidth / window.innerHeight,1,100
+  75 , window.innerWidth / window.innerHeight,1,5000
 )
+camera.position.set(5,10,15)
 
 export default camera
